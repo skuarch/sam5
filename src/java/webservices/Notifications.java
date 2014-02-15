75b1f34bec76d232f5f0d0a5eda96973e255e2e2
skuarch <skuarch@yahoo.com.mx>
2014-02-14 18:56:51 -0600

    forced commit

--- src/java/webservices/Notifications.java
+++ src/java/webservices/Notifications.java
@@ -2,13 +2,19 @@ package webservices;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.util.NoSuchElementException;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.common.BroadcastMessage;
+import model.util.IOUtilities;
 import model.util.JSONUtilities;
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 /**
  *
@@ -31,25 +37,63 @@ public class Notifications extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        PrintWriter out = null;
+        response.setContentType("application/json;charset=UTF-8");
+        JSONObject jsono = null;
+
         try {
-
-            response.setContentType("application/json;charset=UTF-8");
-
-            try (PrintWriter out = response.getWriter()) {
-
-                if (!request.getContentType().equalsIgnoreCase("application/json")) {
-                    out.println(JSONUtilities.getJSONError("illegal content type"));
-                    return;
-                }
-
-                out.print("{\"mocos\":\"mocos\"}");
-
+            
+            if (!request.getContentType().equalsIgnoreCase("application/json")) {
+                logger.error("illegal content type", new Exception());
+                out.println(JSONUtilities.getJSONError("illegal content type"));
+                return;
             }
 
-        } catch (Exception e) {
-            logger.error(e);
+            if (request.getContentLength() < 1) {
+                //nothing to read
+                logger.error("nothing to read", new Exception());
+                return;
+            }
+
+            jsono = new JSONObject(readContent(request));            
+            
+            BroadcastMessage.sendMessage(jsono.getString("description"));
+
+            out = response.getWriter();
+            out.print("{\"received\":\"true\"}");
+
+        } catch (IOException | ParseException | NoSuchElementException e) {
+            logger.error("doPost", e);
+        } finally {
+            IOUtilities.closePrintWriter(out);
         }
 
     } // end doPost
+
+    //==========================================================================
+    private String readContent(HttpServletRequest request) {
+
+        int contentLenght = 0;
+        ServletInputStream sis = null;
+        byte[] buffer = null;
+        int len;
+        String string = null;
+
+        try {
+
+            contentLenght = request.getContentLength();
+            sis = request.getInputStream();
+            buffer = new byte[contentLenght];
+            len = sis.readLine(buffer, 0, buffer.length);
+
+            string = new String(buffer, 0, len);
+
+        } catch (Exception e) {
+            IOUtilities.closeInputStream(sis);
+        }
+
+        return string;
+
+    } // end readContent
 
 } // end class
